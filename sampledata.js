00d9127efca3c56a48b23700054162b144615b8b
Alain Wohlfarth <github@richtungspfeil.de>
2015-07-14 14:04:00 +0200

    Minor

--- sampledata.js
+++ sampledata.js
@@ -2,14 +2,14 @@
 var data = [
     {
         "summary": "Anna Haro’s Birthday",
-        "dtstart": "2013-08-28 22:00:00 +0000",
-        "dtend": "2013-08-29 21:59:59 +0000",
+        "dtstart": "2015-06-28 22:00:00 +0000",
+        "dtend": "2015-06-29 21:59:59 +0000",
         "calendar": "Birthdays"
     },
     {
         "calendar": "Birthdays",
-        "dtstart": "2013-11-08 15:42:00 +0100",
-        "dtend": "2013-11-08 17:44:00 +0100",
+        "dtstart": "2015-11-08 15:42:00 +0100",
+        "dtend": "2015-11-08 17:44:00 +0100",
         "summary": "Demo Bell’s Birthday",
         "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         "organizers": ["Max Mustermann", "Martina Musterfrau"],
@@ -18,26 +18,26 @@ var data = [
     },
     {
         "summary": "Kate Bell’s Birthday",
-        "dtstart": "2014-01-19 23:00:00 +0000",
-        "dtend": "2014-01-20 22:59:59 +0000",
+        "dtstart": "2015-12-19 23:00:00 +0000",
+        "dtend": "2015-12-20 22:59:59 +0000",
         "calendar": "Birthdays"
     },
     {
         "summary": "David Taylor’s Birthday",
-        "dtstart": "2014-06-14 22:00:00 +0000",
-        "dtend": "2014-06-15 21:59:59 +0000",
+        "dtstart": "2015-08-14 22:00:00 +0000",
+        "dtend": "2015-08-15 21:59:59 +0000",
         "calendar": "Birthdays",
     },
     {
         "summary": "John Appleseed’s Birthday",
-        "dtstart": "2014-06-21 22:00:00 +0000",
-        "dtend": "2014-06-22 21:59:59 +0000",
+        "dtstart": "2015-09-21 22:00:00 +0000",
+        "dtend": "2015-09-22 21:59:59 +0000",
         "calendar": "Birthdays"
     },
     {
         "summary": "Anna Haro’s Birthday",
-        "dtstart": "2014-08-28 22:00:00 +0000",
-        "dtend": "2014-08-29 21:59:59 +0000",
+        "dtstart": "2015-08-28 22:00:00 +0000",
+        "dtend": "2015-08-29 21:59:59 +0000",
         "calendar": "Birthdays"
     }
 ];
